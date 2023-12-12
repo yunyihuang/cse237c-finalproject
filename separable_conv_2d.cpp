f8773faa11dfc30e078f7a1f7034c105cea68378
Yunyi Huang <59582716+yuh022@users.noreply.github.com>
2023-12-11 20:10:14 -0800

    correct convolution in C++

--- separable_conv_2d.cpp
+++ separable_conv_2d.cpp
@@ -5,31 +5,28 @@
 // Depthwise convolution
 // Flatten the 2d feature map into 1d array for easier operations
 void depthwise_conv_2d_cl(
-	ap_fixed<10,4> data[in_height * in_width * n_chan],
-	ap_fixed<10,4> depthwise_res[out_height * out_width * n_chan],
-	ap_fixed<10,4> depthwise_weights[filt_height * filt_width * n_chan],
-	ap_fixed<10,4> depthwise_biases[n_chan])
+	int data[in_height * in_width * n_chan],
+	int depthwise_res[out_height * out_width * n_chan],
+	int depthwise_weights[filt_height * filt_width * n_chan],
+	int depthwise_biases[n_chan])
 	{
-		// depthwise output height
-		for (int h = 0; h < out_height; h++) {
-			// depthwise output width
-			for (int w = 0; w < out_width + 1; w++) {
-				// input number of channels
-				for (int c = 0; c < n_chan; c++) {
-					ap_fixed<10,4> sum = depthwise_biases[c];
+		for (int c = 0; c < n_chan; c++) {
+			int channel_start = c * in_height * in_width;
+			int kernel_start = c * filt_height * filt_width;
 
-					// kernel/filter multiplication
+			for (int h = 0; h < out_height; h++) {
+				for (int w = 0; w < out_width; w++) {
+					int sum = depthwise_biases[c];
 					for (int i = 0; i < filt_height; i++) {
 						for (int j = 0; j < filt_width; j++) {
-
-							int data_idx = (h+i) * in_width * n_chan + (w+j) * n_chan + c;
-							int weight_idx = i * filt_width * n_chan + j * n_chan + c;
+							int data_idx = channel_start + (h + i) * in_width + (w + j);
+							int weight_idx = kernel_start + i * filt_width + j;
 							sum += data[data_idx] * depthwise_weights[weight_idx];
 						}
 					}
-					int res_idx = (h * out_width * n_chan) + w * n_chan + c;
+					int res_idx = c * (out_height * out_width) + h * out_width + w;
 					depthwise_res[res_idx] = sum;
-				}
+ 				}
 			}
 		}
 	}
@@ -38,10 +35,10 @@ void depthwise_conv_2d_cl(
 // Pointwise convolution
 // Flatten the 2d feature map into 1d array for easier operations
 void pointwise_conv_2d_latency_cl(
-	ap_fixed<10,4> depthwise_res[out_height * out_width * n_chan],
-	ap_fixed<10,4> res[out_height * out_width * n_filt],
-	ap_fixed<10,4> pointwise_weights[n_chan * n_filt],
-	ap_fixed<10,4> pointwise_biases[n_filt])
+	int depthwise_res[out_height * out_width * n_chan],
+	int res[out_height * out_width * n_filt],
+	int pointwise_weights[n_chan * n_filt],
+	int pointwise_biases[n_filt])
 	{
 		// pointwise output height
 		for (int h = 0; h < out_height; h++) {
@@ -49,15 +46,15 @@ void pointwise_conv_2d_latency_cl(
 			for (int w = 0; w < out_width; w++) {
 				// output number of channels
 				for (int f = 0; f < n_filt; f++) {
-					ap_fixed<10,4> sum = pointwise_biases[f];
+					int sum = pointwise_biases[f];
 
 					// kernel/filter multiplication
 					for (int c = 0; c < n_chan; c++) {
-						int data_idx = (h *out_width * n_chan) + (w * n_chan) + c;
-						int weight_idx = c * n_filt + f;
+						int data_idx = c * (out_height * out_width) + h * out_width + w;
+						int weight_idx = f * n_chan + c;
 						sum += depthwise_res[data_idx] * pointwise_weights[weight_idx];
 					}
-					int res_idx = (h * out_width * n_filt) + (w * n_filt) + f;
+					int res_idx = f * (out_height * out_width) + h * out_width + w;
 					res[res_idx] = sum;
 				}
 			}
@@ -66,13 +63,13 @@ void pointwise_conv_2d_latency_cl(
 
 
 void separable_conv_2d_cl(
-	ap_fixed<10,4> data[in_height * in_width * n_chan],
-	ap_fixed<10,4> depthwise_res[out_height * out_width * n_chan],
-	ap_fixed<10,4> res[out_height * out_width * n_filt],
-	ap_fixed<10,4> depthwise_weights[filt_height * filt_width * n_chan],
-	ap_fixed<10,4> pointwise_weights[n_chan * n_filt],
-	ap_fixed<10,4> depthwise_biases[n_chan],
-	ap_fixed<10,4> pointwise_biases[n_filt]){
+	int data[in_height * in_width * n_chan],
+	int depthwise_res[out_height * out_width * n_chan],
+	int res[out_height * out_width * n_filt],
+	int depthwise_weights[filt_height * filt_width * n_chan],
+	int pointwise_weights[n_chan * n_filt],
+	int depthwise_biases[n_chan],
+	int pointwise_biases[n_filt]){
 
 	depthwise_conv_2d_cl(data, depthwise_res, depthwise_weights, depthwise_biases);
 	pointwise_conv_2d_latency_cl(depthwise_res, res,pointwise_weights, pointwise_biases);
--- separable_conv_2d.h
+++ separable_conv_2d.h
@@ -4,38 +4,38 @@
 const int in_height =3;
 const int in_width = 3;
 // Number of channels
-const int n_chan = 1;
+const int n_chan = 3;
 // Depthwise kernel's height and weight
 const int filt_height = 2;
-const int filt_width = 1;
+const int filt_width = 2;
 // Intermediate matrix's height and weight
 const int out_height = in_height - filt_height + 1;
 const int out_width = in_width - filt_width + 1;
 // Number of output channels
-const int n_filt = 1;
+const int n_filt = 2;
 
 void depthwise_conv_2d_cl(
-	ap_fixed<10,4> data[in_height * in_width * n_chan],
-	ap_fixed<10,4> depthwise_res[out_height * out_width * n_chan],
-	ap_fixed<10,4> depthwise_weights[filt_height * filt_width * n_chan],
-	ap_fixed<10,4> depthwise_biases[n_chan]
+	int data[in_height * in_width * n_chan],
+	int depthwise_res[out_height * out_width * n_chan],
+	int depthwise_weights[filt_height * filt_width * n_chan],
+	int depthwise_biases[n_chan]
 	);
 
 
 void pointwise_conv_2d_latency_cl(
-	ap_fixed<10,4> depthwise_res[out_height * out_width * n_chan],
-	ap_fixed<10,4> res[out_height * out_width * n_filt],
-	ap_fixed<10,4> pointwise_weights[n_chan * n_filt], // shouldn't it always be 1 x 1?
-	ap_fixed<10,4> pointwise_biases[n_filt]
+	int depthwise_res[out_height * out_width * n_chan],
+	int res[out_height * out_width * n_filt],
+	int pointwise_weights[n_chan * n_filt],
+	int pointwise_biases[n_filt]
 	);
 
 
 void separable_conv_2d_cl(
-	ap_fixed<10,4> data[in_height * in_width * n_chan],
-	ap_fixed<10,4> depthwise_res[out_height * out_width * n_chan],
-	ap_fixed<10,4> res[out_height * out_width * n_filt],
-	ap_fixed<10,4> depthwise_weights[filt_height * filt_width * n_chan],
-	ap_fixed<10,4> pointwise_weights[n_chan * n_filt],
-	ap_fixed<10,4> depthwise_biases[n_chan],
-	ap_fixed<10,4> pointwise_biases[n_filt]
+	int data[in_height * in_width * n_chan],
+	int depthwise_res[out_height * out_width * n_chan],
+	int res[out_height * out_width * n_filt],
+	int depthwise_weights[filt_height * filt_width * n_chan],
+	int pointwise_weights[n_chan * n_filt],
+	int depthwise_biases[n_chan],
+	int pointwise_biases[n_filt]
 	);
